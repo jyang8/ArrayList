87d2d10cde2bb131a9e247cfa4e749c23f8f13d4
Jessica <jyang8@stuy.edu>
2016-01-03 12:07:59 -0500

    initial

---
+++ BubbleSort.java
@@ -0,0 +1,120 @@
+/*======================================
+  class BubbleSort -- implements bubblesort algorithm (vanilla version)
+
+  ======================================*/
+
+import java.util.ArrayList;
+
+public class BubbleSort {
+
+    //~~~~~~~~~~~~~~~~~~~ HELPER METHODS ~~~~~~~~~~~~~~~~~~~
+    //precond: lo < hi && size > 0
+    //postcond: returns an ArrayList of random integers
+    //          from lo to hi, inclusive
+    public static ArrayList populate( int size, int lo, int hi ) {
+	ArrayList<Integer> retAL = new ArrayList<Integer>();
+	while( size > 0 ) {
+	    //     offset + rand int on interval [lo,hi]
+	    retAL.add( lo + (int)( (hi-lo+1) * Math.random() ) );
+	    size--;
+	}
+	return retAL;
+    }
+
+    //randomly rearrange elements of an ArrayList
+    public static void shuffle( ArrayList al ) {
+	int randomIndex;
+	//setup for traversal fr right to left
+        for( int i = al.size()-1; i > 0; i-- ) {
+	    //pick an index at random
+            randomIndex = (int)( (i+1) * Math.random() );
+	    //swap the values at position i and randomIndex
+            al.set( i, al.set( randomIndex, al.get(i) ) );
+        }
+    }
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    // VOID version of bubbleSort
+    // Rearranges elements of input ArrayList
+    // postcondition: data's elements sorted in ascending order
+    public static void bubbleSortV( ArrayList<Comparable> data ) {
+
+	for( int passCtr = 1; passCtr < data.size(); passCtr++ ) {
+	    System.out.println( "commencing pass #" + passCtr + "..." );
+
+	    //iterate thru first to next-to-last element, comparing to next
+	    for( int i = 0; i < data.size()-1; i++ ) {
+
+		//if element at i > element at 1+1, swap
+		if ( data.get(i).compareTo(data.get(i+1) ) > 0 ) 
+		    data.set( i, data.set(i+1,data.get(i)) );	
+		
+		//System.out.println(data); //diag: show current state of list
+	    }
+	}
+    }//end bubbleSortV -- O(n*n)
+
+
+    // ArrayList-returning bubbleSort
+    // postcondition: order of input ArrayList's elements unchanged
+    //                Returns sorted copy of input ArrayList.
+    public static ArrayList<Comparable> bubbleSort( ArrayList<Comparable> input ) {
+	//declare and initialize empty ArrayList for copying
+	ArrayList<Comparable> data = new ArrayList<Comparable>();
+
+	//copy input ArrayList into working ArrayList
+	for( Comparable o : input )
+	    data.add( o );
+
+	//sort working ArrayList
+	bubbleSortV( data );
+
+	return data;
+    }//end bubbleSort -- O(n*n)
+
+
+    public static void main(String [] args){
+
+	/*===============for VOID methods=============
+	ArrayList glen = new ArrayList<Integer>();
+	glen.add(7);
+	glen.add(1);
+	glen.add(5);
+	glen.add(12);
+	glen.add(3);
+	System.out.println( "ArrayList glen before sorting:\n" + glen );
+	bubbleSortV(glen);
+	System.out.println( "ArrayList glen after sorting:\n" + glen );
+
+	ArrayList coco = populate( 10, 1, 1000 );
+	System.out.println( "ArrayList coco before sorting:\n" + coco );
+	bubbleSortV(coco);
+	System.out.println( "ArrayList coco after sorting:\n" + coco );
+	  ============================================*/
+
+	/*==========for AL-returning methods==========
+    	ArrayList glen = new ArrayList<Integer>();
+	glen.add(7);
+	glen.add(1);
+	glen.add(5);
+	glen.add(12);
+	glen.add(3);
+	System.out.println( "ArrayList glen before sorting:\n" + glen );
+	ArrayList glenSorted = bubbleSort( glen );
+	System.out.println( "sorted version of ArrayList glen:\n" 
+			    + glenSorted );
+	System.out.println( "ArrayList glen after sorting:\n" + glen );
+
+      	ArrayList coco = populate( 10, 1, 1000 );
+	System.out.println( "ArrayList coco before sorting:\n" + coco );
+	ArrayList cocoSorted = bubbleSort( coco );
+	System.out.println( "sorted version of ArrayList coco:\n" 
+			    + cocoSorted );
+	System.out.println( "ArrayList coco after sorting:\n" + coco );
+	System.out.println( coco );
+	  ============================================*/
+
+    }//end main
+
+}//end class BubbleSort
